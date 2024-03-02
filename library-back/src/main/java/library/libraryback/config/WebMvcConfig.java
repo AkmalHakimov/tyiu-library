70a695b0bcb259283f2a24dd3194ff8826a6c343
Akmal <akmal.hakimov2104@gmail.com>
2024-03-02 09:39:37 +0500

    changes

--- library-back/src/main/java/library/libraryback/config/WebMvcConfig.java
+++ library-back/src/main/java/library/libraryback/config/WebMvcConfig.java
@@ -1,8 +1,20 @@
 package library.libraryback.config;
 
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.ResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
--- library-back/src/main/java/library/libraryback/controller/CategoryController.java
+++ library-back/src/main/java/library/libraryback/controller/CategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/category")
+@CrossOrigin
 public class CategoryController {
 
     private final CategoryService categoryService;
